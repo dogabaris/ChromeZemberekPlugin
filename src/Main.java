5f17197a83b8b7fb1ac76009894951aff24eb667
Doğa Barış <dogabarisozdemir@gmail.com>
2016-03-23 21:35:42 +0200

    sql bağlantısı ++

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.*;
 
 import static spark.Spark.*;
 
-import com.google.common.collect.Interner;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Ordering;
-import jdk.internal.org.objectweb.asm.tree.analysis.Value;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 import net.zemberek.erisim.Zemberek;
 import net.zemberek.tr.yapi.TurkiyeTurkcesi;
 import net.zemberek.yapi.KelimeTipi;
@@ -17,8 +18,26 @@ import zemberek.morphology.apps.TurkishMorphParser;
  * Created by Master on 21.03.2016.
  */
 public class Main  {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException ,ClassNotFoundException {
         port(666);
+
+      /*MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUser("root");
+        dataSource.setPassword("");
+        dataSource.setServerName("127.0.0.1");
+
+        java.sql.Connection conn = dataSource.getConnection();
+        java.sql.Statement stmt = conn.createStatement();
+        java.sql.ResultSet rs = stmt.executeQuery("SELECT ID FROM USERS");*/
+
+        Class.forName("com.mysql.jdbc.Driver");
+        java.sql.Connection baglanti=null;
+        baglanti=DriverManager.getConnection("jdbc:mysql://localhost/googleplugin","root","");
+
+        String query = "insert into frekanslar(kelime,frekans) values('zaa','1');";
+
+        Statement stm = (Statement) baglanti.createStatement();
+        int executeUpdate = stm.executeUpdate(query);
 
         Zemberek z = new Zemberek(new TurkiyeTurkcesi());
         List<Kok> KokList = Lists.newArrayList();
